12cb4cadc895d7f7c9251c39c87b4167a5b9b722
Codekod <meliheken@aof.anadolu.edu.tr>
2025-10-22 23:14:39 +0300

    Fix API folder structure

---
+++ api/delete-account.ts
@@ -0,0 +1,3 @@
+import { createClient } from '@supabase/supabase-js';
+import type { VercelRequest, VercelResponse } from '@vercel/node';
+export default async function handler(req: VercelRequest, res: VercelResponse) { ... }
